4d53382ca1815bd7d715036e8cd59ced858f338a
Gabriel Martins <gabtec@sapo.pt>
2023-02-02 12:57:55 +0000

    refactor(spa): code cleanup

--- src/pages/SpaPage.jsx
+++ src/pages/SpaPage.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import useAuthUser from '../hooks/useAuthUser';
 
 import MainLayout from '../layout/MainLayout';
-// import LoginPage from '../pages/LoginPage';
 
 function SpaPage() {
   const navigate = useNavigate();
@@ -12,25 +11,11 @@ function SpaPage() {
 
   useEffect(() => {
     if (!user) {
-      console.log('run once');
       navigate('/');
     }
-  }, []);
-
-  // return <p>SPA PAGE</p>;
-
-  // console.log('on spa');
-  // console.log('on protected routes');
-  // const { isLoggedIn } = store.getState().auth;
-  // console.log('isLoggedIn: ' + isLoggedIn);
+  }, []); // will run 1x
 
   return <MainLayout user={user || ''}></MainLayout>;
-  // return (
-  //   <>
-  //     {isLoggedIn && <MainLayout />}
-  //     {!isLoggedIn && <LoginPage />}
-  //   </>
-  // );
 }
 
 export default SpaPage;
