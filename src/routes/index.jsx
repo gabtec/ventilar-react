8a54d7b74c123aa3c16172c51d7996fb56d5f2a2
Gabriel Martins <gabtec@sapo.pt>
2023-02-17 14:07:13 +0000

    fix(logout): fixed broken logout feature

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,12 +4,13 @@ import { createBrowserRouter } from 'react-router-dom';
 /* -- Main Pages -- */
 import HomePage from '../pages/HomePage';
 import SpaPage from '../pages/SpaPage';
+import { logoutAction } from '../pages/LogoutPage';
 import NotFound from '../pages/Error404';
 
 /* -- SPA Sub Pages -- */
-import OrdersList from '../components/OrdersList';
+import InitialListByRole from '../pages/SpaSubPages/InitialListByRole';
+import AvailableVentilatorsList from '../pages/SpaSubPages/AvailableVentilatorsList';
 import OrderCreate from '../components/OrderCreate';
-import SpaConsumerAvailableVentsPage from '../pages/SpaSubPages/SpaConsumerAvailableVentsPage';
 
 const router = createBrowserRouter([
   {
@@ -19,7 +20,18 @@ const router = createBrowserRouter([
   {
     path: '/spa',
     element: <SpaPage />,
-    children: [{ index: true, element: <p>Lista conforme o role</p> }],
+    children: [
+      { index: true, element: <InitialListByRole /> },
+      {
+        path: 'ventilators/available/:cat',
+        element: <AvailableVentilatorsList />,
+      },
+      { path: 'orders/create/:cat', element: <OrderCreate /> },
+    ],
+  },
+  {
+    path: '/logout',
+    action: logoutAction,
   },
   {
     path: '*',
